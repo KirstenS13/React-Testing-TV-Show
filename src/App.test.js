52d3b8c26c05014fbe16eba8f605c47ee6e01e9b
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-07-08 20:44:11 -0400

    added pseudocode to Episodes.test.js and App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import App from './App';
+
+test('seasons dropdown menu shows all options and fetches correct episodes and renders them', () => {
+    // Arrange
+        // render App and make sure dropdown menu is shown
+
+    // Act
+        // click on dropdown menu (and make sure options appear)
+        // click on option and (make sure: 
+            // api is called
+            // correct episodes appear)
+
+    // Assert
+        // wait for the api call to finish
+        // make sure dropdown menu options appear
+        // make sure selecting season 
+            // calls api
+            // renders correct episodes
+}); 
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import Episodes from './Episodes';
+
+test('rerenders correctly with the list of episodes', () => {
+    // Arrange
+        // render Episodes component and set up mock data
+        // make sure component is empty on initial render
+
+    // Act
+        // rerender Episodes component with the mock episodes data
+
+    // Assert
+        // make sure episodes are rendered to page correctly
+        // number of episodes should match length of mock data
+});
